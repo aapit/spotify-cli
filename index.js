5d83aea8b87a55661a1de6d5ac590231deb5ae78
David Spreekmeester <david@grrr.nl>
2017-05-20 21:27:06 +0200

    Revert "Fixed tabbing."
    This reverts commit d806e17ced7939aff07c4e7bf58016857ac34ddf.

--- index.js
+++ index.js
@@ -12,85 +12,85 @@ cli.setApp(pkg.name, pkg.version);
 cli.parse(null, ['play', 'pause', 'next', 'previous', 'status']);
 
 function nowPlaying() {
-    client.getTrack(function(err, track) {
-        if (err || !track) cli.error(err);
-        cli.info(track.name + " - " + track.artist);
-    });
+	client.getTrack(function(err, track) {
+		if (err || !track) cli.error(err);
+		cli.info(track.name + " - " + track.artist);
+	});
 }
 
 function parsePlayArg(arg, callback) {
-    if (arg.toLowerCase().substring(0,8) == 'spotify:') return callback(null, arg);
+	if (arg.toLowerCase().substring(0,8) == 'spotify:') return callback(null, arg);
 
-    request('http://ws.spotify.com/search/1/track.json?q=' +  arg, function (err, res, body) {
-        if (err) return callback(err);
-        if (res.statusCode == 200) {
-            callback(null, JSON.parse(body).tracks[0].href);
-        } else {
-            callback(res.statusCode);
-        }
-    });
+	request('http://ws.spotify.com/search/1/track.json?q=' +  arg, function (err, res, body) {
+		if (err) return callback(err);
+		if (res.statusCode == 200) {
+			callback(null, JSON.parse(body).tracks[0].href);
+		} else {
+			callback(res.statusCode);
+		}
+	});
 }
 
 cli.main(function (args, options) {
-    if (cli.command == 'play') {
-        if (cli.args.length) {
-            parsePlayArg(cli.args.join(' '), function (err, url) {
-                client.playTrack(url, function () {
-                    nowPlaying();
-                });
-            });
+	if (cli.command == 'play') {
+		if (cli.args.length) {
+			parsePlayArg(cli.args.join(' '), function (err, url) {
+				client.playTrack(url, function () {
+					nowPlaying();
+				});
+			});
 
-        } else {
+		} else {
 
-            client.play(function () {
-                nowPlaying();
-            });
+			client.play(function () {
+				nowPlaying();
+			});
 
-        }
-    }
+		}
+	}
 
-    if (cli.command == 'pause') {
-        client.pause(function () {
-            cli.ok('paused');
-        });
-    }
+	if (cli.command == 'pause') {
+		client.pause(function () {
+			cli.ok('paused');
+		});
+	}
 
-    if (cli.command == 'next') {
-        client.next(function () {
-            nowPlaying();
-        });
-    }
+	if (cli.command == 'next') {
+		client.next(function () {
+			nowPlaying();
+		});
+	}
 
-    if (cli.command == 'previous') {
-        client.previous(function () {
-            nowPlaying();
-        });
-    }
+	if (cli.command == 'previous') {
+		client.previous(function () {
+			nowPlaying();
+		});
+	}
 
-    if (cli.command == 'status') {
+	if (cli.command == 'status') {
         term.hideCursor();
         progressBar = term.progressBar( {
-            barChar: '░',
-            barHeadChar: '░',
-            percent: true
+			barChar: '░',
+			barHeadChar: '░',
+	        percent: true
         } ) ;
 
         var displayState = function() {
             client.getState(function (err, state) {
-                if (err) return cli.error(err);
-                    client.getTrack(function(err, track) {
-                        if (err) return cli.error(err);
+			    if (err) return cli.error(err);
+			        client.getTrack(function(err, track) {
+				        if (err) return cli.error(err);
                         progress = state.position / (track.duration / 1000);
-                        progressBar.title = track.name;
+						progressBar.title = track.name;
                         progressBar.update({
                             progress: progress,
                             title: track.name + ' - ' + track.artist
                         });
-                    });
-                });
+			        });
+		        });
         };
 
         setInterval(displayState, 1000);
-    }
+	}
 
 });
